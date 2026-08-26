3347713910281cf891501e6f7a696d9c3789eccd
Facundo D'addese <facundo.daddese19@gmail.com>
2026-08-26 20:56:31 -0300

    elimino un console.log de useTip

--- src/hooks/useTip.ts
+++ src/hooks/useTip.ts
@@ -4,7 +4,6 @@ export const useTip = () => {
   const [tipState, setTipState] = useState(0);
 
   const handleTip = (tip: number) => setTipState(tip);
-  console.log(tipState);
 
   return { tipState, handleTip };
 };
